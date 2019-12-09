f0178fa299b63886b4f7b761d28de36f524180eb
Germain Loret <germain.loret@gmail.com>
2019-12-09 16:41:03 +0100

    Install stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+// init stimulus
+const application = Application.start();
+const controllers = require.context('../controllers', true, /\.js$/);
+application.load(definitionsFromContext(controllers));
