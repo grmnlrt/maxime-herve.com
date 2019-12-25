03cce7448095c7c467df2a1e043ab000b8f3c288
Germain Loret <germain.loret@gmail.com>
2019-12-25 21:18:36 +0100

    Create Game and Movie models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_123630) do
+ActiveRecord::Schema.define(version: 2019_12_25_194707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "developer"
+    t.string "editor"
+    t.string "role"
+    t.integer "year"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "director"
+    t.string "production"
+    t.string "genre"
+    t.string "movie_type"
+    t.integer "year"
+    t.string "link"
+    t.text "synopsis"
+    t.text "casting"
+    t.text "informations"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
