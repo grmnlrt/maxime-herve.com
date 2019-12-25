0156295efeff6694f75d0788f39b39507bbb9bdf
Germain Loret <germain.loret@gmail.com>
2019-12-25 21:37:58 +0100

    Create routes to movies and games index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'contact', to: 'pages#contact'
+  get 'informations', to: 'pages#informations'
+  resources :games, only: :index
+  resources :movies, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
