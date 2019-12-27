a3adc190871c711c56fc472c2cc9273ede4557f2
Germain Loret <germain.loret@gmail.com>
2019-12-27 20:48:01 +0100

    Finish movie card design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def truncate(string, max_length)
+    str = string.to_s
+    if str.length > max_length
+      str[0...max_length] + "..."
+    else
+      str
+    end
+  end
 end
