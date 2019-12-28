5ac4b60aaa8fbf54f811b9775125b400716f720d
Germain Loret <germain.loret@gmail.com>
2019-12-28 17:10:49 +0100

    Send email each time a new message is created

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,7 @@
 class MessageMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.message_mailer.new.subject
-  #
   def new
     @message = params[:message]
 
-    mail to: AdminUser.find(send_emails: true).email
+    mail to: AdminUser.find_by(send_emails: true).email
   end
 end
