b3269e6e012db01fd2cd3b36c96d87739fa49990
Germain Loret <germain.loret@gmail.com>
2019-12-30 16:19:07 +0100

    Order movies and and rewards on index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_212845) do
+ActiveRecord::Schema.define(version: 2019_12_30_145923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_12_27_212845) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "send_emails", default: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
@@ -67,8 +68,18 @@ ActiveRecord::Schema.define(version: 2019_12_27_212845) do
     t.string "developer"
     t.string "editor"
     t.string "role"
-    t.integer "year"
+    t.string "year"
     t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "genre"
+    t.string "franchise"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "email"
+    t.string "subject"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -86,18 +97,7 @@ ActiveRecord::Schema.define(version: 2019_12_27_212845) do
     t.text "informations"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.integer "priority", default: 1
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
