c3328852b4e0bfcb487305e02509825c0d9f4afd
Benjamin Leray <b.leray35@gmail.com>
2019-12-30 15:45:34 +0100

    8 points rules

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_172025) do
+ActiveRecord::Schema.define(version: 2019_12_30_142705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2019_12_29_172025) do
     t.datetime "updated_at", null: false
     t.string "genre"
     t.string "year"
-    t.string "franchise"
   end
 
   create_table "movies", force: :cascade do |t|
